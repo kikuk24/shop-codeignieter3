55e07ac93b47b3223c6a1bd43332d10cc7877ace
kikuk24 <tirta.afandi24@gmail.com>
2023-10-07 19:22:15 +0700

    create: Logout

---
+++ application/controllers/Logout.php
@@ -0,0 +1,24 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Logout extends My_Controller {
+
+  public function index()
+  {
+    $sess_data  = [
+      'id', 'name','email','role', 'is_login' 
+    ];
+    
+    $this->session->unset_userdata($sess_data);
+    
+    $this->session->sess_destroy();
+    redirect(base_url());
+    
+    
+  }
+
+}
+
+/* End of file Logout.php */
